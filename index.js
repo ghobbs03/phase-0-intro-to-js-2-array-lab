d2266641b34da8d630b5ab7ce9e789478c853c96
Gail Hobbs <gbhobbs97@gmail.com>
2023-01-03 15:55:14 -0800

    commit tree 4003a2182664fa59adac1142b2a44a01cbf687cd

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+};
+
+function appendCat(name) {
+    const copycatsAppend= [...cats, name];
+    return copycatsAppend;
+};
+
+function prependCat(name) {
+    const copycatsPrepend = [name,...cats];
+    return copycatsPrepend;
+};
+
+function removeLastCat() {
+    const copycatsRemoveLast = cats.slice(0,cats.length - 1);
+    return copycatsRemoveLast;
+};
+
+function removeFirstCat(){
+    const copycatsRemoveFirst = cats.slice(1);
+    return copycatsRemoveFirst;
+};
+
+
